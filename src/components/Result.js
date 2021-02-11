02be9626ddf832a35bfda70e64edfed58d2b7979
cgraaaj <cgrvel@gemail.com>
2021-02-11 12:33:50 +0530

    search word to lower case

--- src/components/Result.js
+++ src/components/Result.js
@@ -12,12 +12,10 @@ const Result = ({ words, audio, sword }) => {
       </div>
     </div>
   ) : (
-    <div className="five wide column">
-      <img
-        src="https://66.media.tumblr.com/20d9f53297a8984a7455d6f5bc336c58/e05115941f982421-ff/s500x750/72493d1229f97a31dddb0fb58a391724cb3e2609.gif"
-        alt="thats not even a word"
-      ></img>
-    </div>
+    <img
+      src="https://66.media.tumblr.com/20d9f53297a8984a7455d6f5bc336c58/e05115941f982421-ff/s500x750/72493d1229f97a31dddb0fb58a391724cb3e2609.gif"
+      alt="thats not even a word"
+    ></img>
   );
 };
 
--- src/components/WordList.js
+++ src/components/WordList.js
@@ -1,19 +1,19 @@
-import React from 'react';
-import WordItem from './WordItem';
+import React from "react";
+import WordItem from "./WordItem";
 
 const WordList = ({ words, sword }) => {
-   const renderedList = words.map((word) => {
-      if(word.meta.id.includes(sword)) {
+  const renderedList = words.map((word) => {
+    if (word.meta.id.includes(sword.toLowerCase())) {
       return <WordItem key={word.meta.uuid} word={word} />;
-      }
-      return null
-   });
-   return (
-      <div>
-         {/* <div className="ui relaxed divided list">{renderedList}</div> */}
-         <div className="ui cards ui">{renderedList}</div>
-      </div>
-   );
+    }
+    return null;
+  });
+  return (
+    <div>
+      {/* <div className="ui relaxed divided list">{renderedList}</div> */}
+      <div className="ui cards ui">{renderedList}</div>
+    </div>
+  );
 };
 
 export default WordList;
