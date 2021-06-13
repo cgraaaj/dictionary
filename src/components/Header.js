4557c54785e80ff7a60059c0137b2151e98cec06
pudge <cgrvel@gmail.com>
2021-06-14 02:24:47 +0530

    btn

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,7 +7,7 @@ class Header extends React.Component {
     return (
       <div className="ui secondary  menu">
         <Link to="/books" className="item">
-          BooooBs
+          Books
         </Link>
         <Link to="/" className="item">
           Dictionary
